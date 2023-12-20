7b81ae2d882e78e32ebdd587fbe06d2dfb0b51f5
Vikaskumarrr <143922604+Vikaskumarrr@users.noreply.github.com>
2023-12-21 01:45:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //your JS code here. If required.
-function dayOfYear() {
-	if(dayOfYear % 2 == 0){
-		if(dayOfYear % 400 === 0 && dayOfYear % 100 === 0){
+function dayOfYear(year) {
+	let isyar = false;
+	if(year % 2 == 0){
+		if(year % 400 === 0 && year % 100 === 0){
 			return 366;
 		}
 	}  return 365;
